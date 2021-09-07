52468af4572e78fae9d05d34f36ea0741ccc5183
Michał Jabłoniec <michaljabloniec@gmail.com>
2021-09-07 12:34:10 +0200

    ćwiczenie w dodaniu nowego commita

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world do zadania 1");
+        System.out.println("Dodanie nowego commita");
     }
 }
